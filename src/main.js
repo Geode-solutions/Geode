27bc6b82b201a128cb34d5418753a5740ef9fd46
Arnaud Botella <arnaud.botella@univ-lorraine.fr>
2019-01-18 11:42:49 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from "vue";
+import './plugins/vuetify'
+import App from "./App.vue";
+import store from "./store";
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+Vue.config.productionTip = false;
+
+new Vue({
+  store,
+  render: h => h(App)
+}).$mount("#app");
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+import 'vuetify/src/stylus/app.styl'
+import colors from 'vuetify/es5/util/colors'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.lightBlue.base,
+    secondary: colors.cyan.darken1,
+    accent: colors.red.darken4,
+    info: colors.teal.lighten1,
+    warning: colors.amber.base,
+    error: colors.deepOrange.accent4,
+    success: colors.green.accent3
+  },
+  iconfont: 'md',
+})
