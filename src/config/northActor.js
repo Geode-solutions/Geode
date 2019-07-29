10bfd6f0fd30c776b0cd2658797320737f754c22
Arnaud Botella <botella.arnaud@gmail.com>
2019-07-29 14:09:52 +0200

    Add license

--- src/config/northActor.js
+++ src/config/northActor.js
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2019 Geode-solutions
+ *
+ * This file is a part of Geode library.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
 import macro from "vtk.js/Sources/macro";
 import vtkDataArray from "vtk.js/Sources/Common/Core/DataArray";
 import vtkActor from "vtk.js/Sources/Rendering/Core/Actor";
