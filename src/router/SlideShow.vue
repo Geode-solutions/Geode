938a7299e9129e91680f0392c7f957e32e84f673
panquez <pierre.anquez3@gmail.com>
2019-08-29 11:02:39 +0200

    struct for presentation

--- src/router/SlideShow.vue
+++ src/router/SlideShow.vue
@@ -17,15 +17,94 @@ Lesser General Public License for more details.
 <template>
   <div class="reveal">
     <div class="slides">
-      <section>Single Horizontal Slide</section>
+      <section><h3 class="display-3 py-3">
+        <span class="display-4 primary--text">OpenGeode </span><br/>
+         an industrial open source geosciences meshing framework</h3>
+        <span class="display-1"><b>Arnaud Botella</b> and Pierre Anquez</span></br>
+        <span class="display-2 primary--text">Geode-solutions</span>
+        <img src="/assets/logo.png" height="200" width="200"></img>
+      </section>
+      
+      <section>I - Why another data model dedicated to Geosciences?
+        "State of the art"
+      </section>
+      <section>BRep for CAD
+        besoins CAD : pour faire un maillage - scellé...
+      </section>
+      <section>Special needs for Geosciences</section>
+      <section>First attempts</br>
+        GeoMesh: JP, one article, 2013 - 2015<br/>
+        RINGMesh: RING, RING team research, 2014 - 2017
+      </section>
+      <section>Key points
+        <ul>
+          <li>Geosciences</li>
+          <li>Industrial quality</li>
+          <li>Open source</li>
+        </ul>
+      </section>
+
+      <section>II - OpenGeode</section>
       <section>
-        <section>Vertical Slide 1</section>
+        <body>Industrial quality</body>
+        <body>CI/CD</body>
+      </section>
+      <section>
+        <body>Multi-platform</body>
+        <body>Pre-compiled binaries for Linux, Windows and macOS</body>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/1200px-Apple_logo_black.svg.png"  height="200" width="200">
+      </section>
+            <section>
+        <body>Extensible</body>
+        <body>Modules</body>
+      </section>
+      <section>
+        <section>Redesigned UI
+            <body>JS, web based</body>
+        </section>
         <section style="height: 100%">
           <vtk-view />
         </section>
       </section>
-      <section>Single Horizontal Slide</section>
-      <section>Single Horizontal Slide</section>
+
+      <section>III - BRep in OpenGeode</section>
+      <section>
+        <section>Assembly of Components
+          <body>Corners, Lines, Surfaces, Blocks</body>
+        </section>
+        <section style="height: 100%">
+          <vtk-view />
+        </section>
+      </section>
+      <section>
+        <section>Data structure
+          <body>Mixins</body>
+        </section>
+        <section>
+          Show code
+          <pre><code>
+            namespace geode
+            {
+                template < index_t dimension, template < index_t > class... Components >
+                class GeoRepresentation : public Components< dimension >...
+                {
+                  // methods
+                };
+                class BRep : public GeoRepresentation< 3, Corners, Lines, Surfaces, Blocks >
+                {
+                  // methods
+                };
+            }
+          </code></pre>
+        </section>
+      </section>
+      <section>
+        Non continuous indexing - UUID</br>
+        Model edition 
+      </section>
+
+      <section>IV - Demo</section>
+      
     </div>
   </div>
 </template>
@@ -42,16 +121,27 @@ export default {
     VtkView
   },
   mounted() {
-    Reveal.initialize();
+    Reveal.initialize({slideNumber: true});
   }
 };
 </script>
 
 <style>
+.reveal {
+  font-family: Roboto;
+}
 .reveal .controls {
   color: var(--v-primary-base);
 }
 .reveal .progress {
   color: var(--v-primary-base);
+}
+.reveal .slide-number {
+  left: 8px;
+  color: var(--v-primary-base);
+  background-color: rgba(0,0,0,0);
+}
+.reveal section img {
+  border: 0px;
 }
 </style>
