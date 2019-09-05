fbef6d4e0d5e5e02674f0a0b7102ccd893111157
panquez <pierre.anquez3@gmail.com>
2019-09-05 12:29:54 +0200

    update

--- src/router/SlideShow.vue
+++ src/router/SlideShow.vue
@@ -31,12 +31,14 @@ Lesser General Public License for more details.
       </section>
       <section> 
         <span class="slidetitle">Geomodeling goal is understand subsurface</span><br/> 
-        <p>Geological model</p> 
-        <p class="sub-item">Describe geometry and properties</p> 
-        <!-- Add vertical space -->
-        <p>Physical simulations</p> 
-        <p class="sub-item">Response to these geometry and properties</p> 
-        <p class="message"> Meshes discretize model geometry and properties for numerical computations </p>
+
+        <v-layout style="position:relative; width:800px; height:200px; margin:0 auto;" class="justify-center">
+        <img style="position:absolute;top:0;left:0;" src="@/assets/geomodeling1.svg">
+        <img class="fragment" style="position:absolute;top:0;left:0;" src="@/assets/geomodeling2.svg">
+        </v-layout>
+        <p class="caption">Model from Caumon et al., Math Geosci, 2009</p>
+        <p class="caption">Mesh and simulation from Botella, 2016</p>
+        <p class="message fragment"> Meshes discretize model geometry and properties for numerical computations </p>
       </section>
       <section>
         <span class="slidetitle">Boundary Representations</span><br/>
@@ -45,61 +47,74 @@ Lesser General Public License for more details.
 
         <p>BRep <b class="primary--text">required</b> for meshing solids</p>
         <ul>
-          <li class="sub-item">Mesh conformity</li>
-          <li class="sub-item">Tags on mesh elements</li>
+          <li>Mesh conformity</li>
+          <li>Tags on mesh elements</li>
         </ul>
       </section>
       <section>
         <p class="slidetitle">Specific needs for Geosciences</p>
         <div class="fragment">
           <p> Multi-materials models </p>
-          <img src="@/assets/faraj_2016.png" height="200">
+          <img src="@/assets/faraj_2016.png" height="180">
           <p class="caption">Faraj et al., Computers & Graphics, 2016</p>
         </div>
         <div class="fragment">
           <p> Internal features (fractures, faults) </p>
-          <img src="@/assets/caumon_2009.png" height="200">
-          <p class="caption">Caumon et al., Math Geosci, 2009</p>
+          <img src="@/assets/dfn_jp2014.png" height="180">
+          <p class="caption">Image from Pellerin, 2014. Model courtesy of RING Consortium</p>
         </div>
       </section>
 
       <section>
         <p class="slidetitle">SDK providing data model for geological BReps</p>
-        RINGMesh - SKUA-Gocad - Geo3DML
+        <v-row class="justify-center">
+          <img src="@/assets/skua.png" height="180">
+          <img src="@/assets/ringmesh.png" height="180">
+          <p>Geo3DML</p>
+        </v-row>
+        <v-row class="justify-center space-between">
+          <img src="@/assets/emerson.png" height="100">
+          <img src="@/assets/logo_ring.png" height="100">
+          <p>China Geological Survey (CGS)</p>
+        </v-row>
       </section>
       <section>
         <p class="slidetitle">Required features</p>
         <p class="py-5">Need for a data model that is</p>
         <ul>
-          <li class="fragment sub-item">Dedicated for Geosciences</li>
-          <li class="fragment sub-item">Reliable (industrial quality)</li>
-          <li class="fragment sub-item">Handy (open, flexible, ...)</li>
+          <li class="fragment">Dedicated for Geosciences</li>
+          <li class="fragment">Reliable (industrial quality)</li>
+          <li class="fragment">Handy (open, flexible, ...)</li>
         </ul>
         <p class="message fragment py-5">No data model <b>combines</b> these three points.</p>
       </section>
       <section>
         <p class="slidetitle">Our proposal</p>
-        <img src="@/assets/inheritance.svg" >
+        <v-layout style="position:relative; width:800px; height:800px; margin:0 auto;" class="justify-center">
+        <img class="fragment fade-out" data-fragment-index="0" style="position:absolute;top:0;left:0;" src="@/assets/attempt.svg">
+        <img class="fragment" data-fragment-index="0" style="position:absolute;top:0;left:0;" src="@/assets/inheritance.svg">
+        </v-layout>
       </section>
 
       <section class="sectiontitle">I - OpenGeode specifications</section>
       <section>
-        <section>
           <p class="slidetitle">Industrial quality</p>
-          <!-- <p>What is needed for ?</p> -->
-          <p class="fragment">Reliability</p>
-          <!-- <p class="fragment">Cross-platform</p>
-          <p class="fragment">Various exchanges</p> -->
+        <section>
+          <p >What are the objectives?</p>
+          <ul>
+            <li class="fragment">Reliable</li> 
+            <li class="fragment">Multi-platform</li> 
+          </ul>
         </section>
         <section>
-          <p class="slidetitle">Industrial quality</p>
           <p>Code quality</p>
-          <p class="sub-item">Modern programming (CMake, C++11, ...)</p>
-          <p class="sub-item">Clean & readable code</p>
-          <p class="sub-item">Concurrency programming</p>
+          <ul>
+          <li class="fragment">Modern programming (CMake, C++11, ...)</li>
+          <li class="fragment">Clean & readable code</li>
+          <li class="fragment">Concurrency programming</li>
+          </ul>
         </section>
         <section>
-          <p class="slidetitle">Industrial quality</p>
           <p>Multi-platform</p>
           <p class="sub-item">Pre-compiled binaries for <br/> Linux, Windows and macOS</p>
           <img src="@/assets/linux.jpeg" height="150">
@@ -107,22 +122,25 @@ Lesser General Public License for more details.
           <img src="@/assets/apple.png" height="150">
         </section>
         <section>
-          <p class="slidetitle">Industrial quality</p>
           <p>Continuous Integration</p>
           <p class="sub-item">Tests, code coverage ~ 90%</p>
+          <img src="@/assets/ci.png" height="200">
+          <div class="fragment">
           <p>Continuous Deployment</p>
           <p class="sub-item">Using GitHub and Azure DevOps</p>
+          <img src="@/assets/cd.png" height="100">
+          <img src="@/assets/Azure-pipelines.png" height="100">
+          </div>
         </section>
       </section>
       <section>
-        <section>
           <p class="slidetitle">Open model</p>  
+        <section>
           <p>Complete framework is accessible on GitHub</p>
           <p class="sub-item">Very permissive License (MIT)</p>
             <img src="@/assets/Github_GS.png" height="300"> 
         </section>
         <section>
-          <p class="slidetitle">Open model</p>
           <p>Usage of open source dependencies:</p>
           <ul>
             <li>Async++: concurrency programming</li>
@@ -132,7 +150,6 @@ Lesser General Public License for more details.
           </ul>
         </section>
         <section>
-          <p class="slidetitle">Open model</p>
           <p>Create an open source ecosystem:</p>
           <ul>
             <li>Open the door to contributions</li>
@@ -145,13 +162,17 @@ Lesser General Public License for more details.
       <section>
         <p class="slidetitle">Extensibility</p>
         <p>Providing tools for creating modules</p>
-        <p class="sub-item">Template repository</p>
-        <p> flexible data structures</p>
-        <img src="@/assets/lego.jpg" height="250">
+        <ul>
+          <li>Template repository</li>
+          <li>Flexible data structures</li>
+        </ul>
+        <div> 
+          <img src="@/assets/lego.jpg" height="250">
+        </div>
       </section>
       <section>
+          <p class="slidetitle">Redesigned UI</p>
         <section>
-          <p class="slidetitle">Redesigned UI<p>
           <p>Using JavaScript and web-based technologies</p>
           <img src="@/assets/vue.png" height="150" class="mx-5"> 
           <img src="@/assets/vuetify.jpeg" height="150" class="mx-5">
@@ -163,53 +184,68 @@ Lesser General Public License for more details.
       <section>
         <p class="slidetitle">Scriptability</p>
         <p>Providing JavaScript wrapper</p>
-        <p class="sub-item">Rapid prototyping</p>
-        <p class="sub-item">UI interaction from JavaScript to C++</p>
-        <img src="@/assets/lego.jpg" height="250">
+        <ul>
+          <li>Rapid prototyping</li>
+          <li>UI interaction from JavaScript to C++</li>
+        </ul>
+        <div>
+          <img src="@/assets/script.jpg" height="250">
+        </div>
       </section>
 
       <section class="sectiontitle">II - OpenGeode features</section>
       <section>
-        <section>
           <p class="slidetitle">Objects are assemblies of Components</p>
-          <p class="sub-item">Corners, Lines, Surfaces, Blocks</p>
+        <section>
+          <p>Provided components</p>
+          <ul>
+           <li>Corners</li>
+           <li>Lines</li>
+           <li>Surfaces</li>
+           <li>Blocks</li>
+           <li>Faults</li>
+           <li>...</li>
+          </ul>
         </section>
         <section style="height: 100%">
           <vtk-view view-type="View3D:assembly"/>
         </section>
       </section>
       <section>
-        <section>
           <p class="slidetitle">Assembly design</p>
           <p>Mixin pattern</p>
+        <section>
+          <img class="elevation-10" src="@/assets/code1.svg">
         </section>
         <section>
-          <pre class="cpp"><code data-trim>
-          template < index_t dimension, template < index_t > class... Components >
-          class GeoRepresentation : public Components< dimension >...
-          {
-            // methods
-          };
-          class BRep : public GeoRepresentation< 3, Corners, Lines, Surfaces, Blocks >
-          {
-            // methods
-          };
-          </code></pre>
+          <img class="elevation-10" src="@/assets/code2.svg">
         </section>
       </section>
       <section>
         <p class="slidetitle">Ease model editing</p> 
         <p>Non continuous indexing</p>
-        <p class="sub-item">Topology stored as a graph</p>
-        <p class="sub-item">Components identified by UUIDs</p>
-
+        <ul>
+          <li>Topology stored as a graph</li>
+          <li>Components identified by UUIDs</li>
+        </ul>
+        <div>
+          <img src="@/assets/remove.svg" height="400px"> 
+        </div>
       </section>
 
       <section><p class="sectiontitle">III - Short demo</p>
-      <p>Objectives: Graphical User Interface, extensibility, scriptability</p></section>
+        <p>Objectives</p>
+        <ul>
+          <li>Graphical User Interface</li>
+          <li>Extensibility</li>
+          <li>Scriptability</li>
+        </ul>
+      </section>
 
 
-      <section class="slidetitle">Executed code</section>
+      <section class="slidetitle">Executed code
+        TODO
+      </section>
 
       <section class="sectiontitle">Conclusions and perspectives</section>
       <section>OpenGeode is designed to be a C++ framework
@@ -223,7 +259,7 @@ Lesser General Public License for more details.
       </section>
 
       <section> <p class="slidetitle">Perspectives</p>
-              <section>
+        <section>
           <p >Export/Import</p>
           <p class="sub-item">From/to geological standard RESQML</p>
         </section>
@@ -233,7 +269,7 @@ Lesser General Public License for more details.
         <p class="sub-item">Soon open sourced (by the end of 2019)</p>
         <div class="fragment">
           <p class="sub-item">More information when ready on Twitter</p>
-          <img src="@/assets/Twitter_GS.jpg" height="300"/> 
+          <img src="@/assets/Twitter_GS.jpg" height="300"> 
         </div>
       </section>
       </section>
@@ -248,6 +284,7 @@ Lesser General Public License for more details.
       
       <section>
         <p class="slidetitle">Thank you for your participation</p>
+        <img src="@/assets/social_logos.svg" height="30"/> 
       </section>
       
     </div>
@@ -296,6 +333,11 @@ export default {
   margin-left: -2em; 
 }
 
+.reveal section li, .sub-item {
+  font-size:smaller;
+	font-weight: light;
+  color : rgb(82, 90, 99);
+}
 
 .reveal .controls {
   color: var(--v-primary-base);
@@ -321,16 +363,12 @@ export default {
 }
 .sectiontitle {
 	font-weight: bolder;
+  font-size: larger;
   color: brown;
 }
 .slidetitle {
 	font-weight: bolder;
   color: var(--v-primary-base);
-}
-.sub-item {
-  font-size:smaller;
-	font-weight: light;
-  color : slategrey;
 }
 .message {
   font-size:larger;
